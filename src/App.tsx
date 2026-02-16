9107acd5c16d86f8790d56f0dc0c891e1089aefc
Favortech <davidabolarinwa4@gmail.com>
2026-02-16 14:19:39 +0100

    the commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
-import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
